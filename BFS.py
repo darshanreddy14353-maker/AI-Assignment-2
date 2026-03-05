3ed46ac3aa7072a069caee066d4ed5d1f6cfbb38
Chaitra Darshan <darshanreddy14353@gmail.com>
2026-03-05 21:39:17 +0530

    Create BFS.py

---
+++ BFS.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+def bfs(graph, start):
+
+    visited = []
+    queue = deque([start])
+
+    while queue:
+
+        node = queue.popleft()
+
+        if node not in visited:
+            visited.append(node)
+            queue.extend(graph[node])
+
+    return visited
+
+
+graph = {
+    'A': ['B','C'],
+    'B': ['D','E'],
+    'C': ['F'],
+    'D': [],
+    'E': [],
+    'F': []
+}
+
+print("BFS Traversal:", bfs(graph,'A'))
